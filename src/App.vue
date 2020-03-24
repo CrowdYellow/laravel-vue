7bd5be84b8a3b2db35cc18c9d44055707d34816b
CrowdYellow <echo61546@gmail.com>
2020-03-24 13:46:01 +0800

    v1.0.4 添加头部

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,44 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+  <div id="wrap">
+    <TheHeader/>
     <router-view/>
   </div>
+<!--  <div id="app">-->
+<!--    <TheHeader/>-->
+<!--&lt;!&ndash;    <div id="nav">&ndash;&gt;-->
+<!--&lt;!&ndash;      <router-link to="/">Home</router-link> |&ndash;&gt;-->
+<!--&lt;!&ndash;      <router-link to="/about">About</router-link>&ndash;&gt;-->
+<!--&lt;!&ndash;    </div>&ndash;&gt;-->
+<!--    <router-view/>-->
+<!--  </div>-->
 </template>
 
-<style lang="scss">
-$icon-font-path: "~bootstrap-sass/assets/fonts/bootstrap/";
-$fa-font-path: "~font-awesome/fonts/";
-@import "~bootstrap-sass/assets/stylesheets/_bootstrap";
-@import "~font-awesome/scss/font-awesome";
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+import TheHeader from "@/components/layouts/TheHeader";
+export default {
+  name: "App",
+  components: {
+    TheHeader
   }
 }
+</script>
+
+<style lang="scss">
+$container-large-desktop: 1200px;
+$btn-primary-bg: #00b5ad;
+$btn-primary-border: #00b5ad;
+$label-primary-bg: #00b5ad;
+$pagination-active-bg: #00b5ad;
+$pagination-active-border: #00b5ad;
+$pagination-color: #00b5ad;
+$input-border-focus: #00b5ad;
+$link-color: #12c4c5;
+$link-hover-color: #22ddde;
+$icon-font-path: "~bootstrap-sass/assets/fonts/bootstrap/";
+$fa-font-path: "~font-awesome/fonts/";
+
+@import "~bootstrap-sass/assets/stylesheets/_bootstrap";
+@import "~font-awesome/scss/font-awesome";
+@import "./styles/main";
+@import "./styles/extra";
 </style>
---
+++ src/components/layouts/TheHeader.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="navbar navbar-default topnav">
+        <div class="container">
+            <div class="navbar-header">
+                <router-link to="/" class="navbar-brand">
+                    <img :src="logo.src" :alt="logo.title">
+                    <span class="title">{{ logo.title }}</span>
+                </router-link>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TheHeader',
+    data() {
+        return {
+            logo: {
+                src: `${this.uploadsUrl}Treasure/blob/master/icon.png?raw=true`,
+                title: 'HQ-BLOG'
+            }
+        }
+    },
+    beforeCreate() {
+        this.uploadsUrl = 'https://github.com/CrowdYellow/'
+    }
+}
+</script>
+
+<style scoped>
+.title {
+    color: #1b1a1a;
+}
+</style>
