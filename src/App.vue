ee3580ae05c966850bf370a062bfd88d254bedb5
CrowdYellow <echo61546@gmail.com>
2020-03-24 13:19:29 +0800

    v1.0.2 安装sass和bootstrap-sass

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,22 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
+    <div class="container">
+      <div class="jumbotron">
+        <div class="container">
+          <h1>Welcome from Bootstrap</h1>
+          <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+          <p><a class="btn btn-primary btn-lg" href="http://www.bootcss.com/" role="button" target="_blank">Learn more <span class="glyphicon glyphicon-hand-right" aria-hidden="true"></span></a></p>
+        </div>
+      </div>
+    </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+$icon-font-path: "~bootstrap-sass/assets/fonts/bootstrap/";
+@import "~bootstrap-sass/assets/stylesheets/_bootstrap";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
